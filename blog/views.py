9131ccf0a98b9b47cb4952c5999bf4e09ac621ef
Infamous003 <arfathsyed0312@gmail.com>
2025-04-29 18:12:51 +0530

    Add post_list view and templating to index.html

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, HttpResponse
+from .models import Post
 # Create your views here.
+
+def post_list(request):
+    posts = Post.objects.all()
+    return render(request, 'index.html', {'posts': posts})
