ff47348bead3f18d6d054e34dd8d3fbc1758a682
Infamous003 <arfathsyed0312@gmail.com>
2025-05-05 12:51:35 +0530

    Add login and logout functionality

--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import AuthenticationForm
 from .forms import CustomUserForm
 # Create your views here.
 
@@ -9,8 +10,23 @@ def user_register(request):
         form = CustomUserForm(request.POST)
         print(form.is_valid(), "-----------------------------")
         if form.is_valid():
-            form.save()
-            return redirect('/') #name of the app : url name
+            # form.save() # also returns the user value/object
+            login(request, form.save())
+            return redirect('/')
     else:
         form = CustomUserForm()
     return render(request, 'users/register.html', {'form': form})
+
+def user_login(request):
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            login(request, form.get_user()) # form.get_user() gets the user obviously
+            return redirect('/')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'users/login.html', {'form': form})
+
+def user_logout(request):
+    logout(request)
+    return redirect('/')
